51e82b850250dc764eabe51c3d8b683ffac3d3ac
Renato <rossorene4@gmail.com>
2024-05-13 18:06:24 -0300

    coloquei o calendário

--- sign.js
+++ sign.js
@@ -4,10 +4,18 @@ mostrarSenha.addEventListener("click", (evt) => {
   let inputSenha = document.getElementById("exampleInputPassword1");
 
   if (inputSenha.type == "password") {
-    mostrarSenha.textContent = "Esconder senha"
+    if (mostrarSenha.classList.contains("fa-eye-low-vision")){
+      mostrarSenha.classList.remove("fa-eye-low-vision");
+      mostrarSenha.classList.remove("fa-sharp");
+      mostrarSenha.classList.add("fa-eye");
+    }
     inputSenha.type = "text";
   } else {
-    mostrarSenha.textContent = "Mostrar senha";
+    if (mostrarSenha.classList.contains("fa-eye")){
+      mostrarSenha.classList.add("fa-eye-low-vision");
+      mostrarSenha.classList.add("fa-sharp");
+      mostrarSenha.classList.remove("fa-eye");
+    }
     inputSenha.type = "password";
   }
 });
