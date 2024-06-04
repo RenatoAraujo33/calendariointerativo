20ff69161615c2b2066bdb75cd5c504fc559c905
Rento <rossorene4@gmail.com>
2024-06-04 18:20:46 -0300

    alterações

--- PHP/compromisso.php
+++ PHP/compromisso.php
@@ -29,14 +29,6 @@
               <li class="list-group-item">Compromisso:</li>
             </ul>
           </div>
-          <div class="card" style="width: 18rem; margin-top: 5%;">
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item">Data:</li>
-              <li class="list-group-item">Hora:</li>
-              <li class="list-group-item">Compromisso:</li>
-            </ul>
-          </div>
-        </div>
         <nav class="navbar bg-body-tertiary" style="width: 100%;">
           <div class="container-fluid">
             <a class="tarefas" href="compromisso.html">
---
+++ PHP/insert_compromisso.php
@@ -0,0 +1,33 @@
+<?php
+$nomeServidor = "localhost";
+$nomeUsuario = "root"; //Usuário banco (root)
+$password = "root"; //Senha root MySQL
+$nomeBanco = "db_calendario_interativo";
+
+// A linha abaixo é o comando para se conectar no banco usando a função mysqli_connect
+
+$conexao = mysqli_connect($nomeServidor,$nomeUsuario, $password,$nomeBanco);
+
+// Esse comando é para verificar se a conexão foi feita com sucesso
+if (!$conexao) {
+    die("falha na conexão: " . mysqli_connect_error());
+}
+
+//Recebendo dados do formulário
+
+$data = $_POST['data'];
+$hora = $_POST['hora'];
+$mensagem = $_POST['mensagem'];
+$id_usuario = "1";
+
+$sql = "INSERT INTO tb_compromisso VALUES (null,'$data','$hora','$mensagem','$id_usuario')";
+
+if (mysqli_query($conexao, $sql)) {
+    echo "<script> alert('Usuário cadastrado com sucesso'); location.href='../calendario.html'; </script>";
+
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conexao);
+}
+
+mysqli_close($conexao);
+?>
