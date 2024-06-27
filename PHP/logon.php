4dfa273225b439b8feaea7fc03582abe7dc84179
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-06-26 22:30:57 -0300

    teste

--- PHP/logon.php
+++ PHP/logon.php
@@ -3,7 +3,11 @@ include 'conexao.php';
 
 // A linha abaixo é o comando para se conectar no banco usando a função mysqli_connect
 
-$conexao = mysqli_connect($nomeServidor,$nomeUsuario, $password,$nomeBanco);
+//$conexao = mysqli_connect($nomeServidor,$nomeUsuario, $password,$nomeBanco);
+
+// A linha abaixo é a sessão via php
+
+session_start();
 
 //essas variávaeis abaixo estão armazenando os dados vindo do usuário
 $email = $_POST['email'];
@@ -19,11 +23,9 @@ $_resultado = mysqli_fetch_array($_query);
 
 //essas duas variáveis estão armazenando o e-mail e a senha vindas do banco
 $email_banco = $_resultado['email'];
-$senha_banco = $_resultado ['senha'];
-
+$senha_banco = $_resultado['senha'];
 
 if ($email == $email_banco && $senha == $senha_banco) {
-    session_start();
     $_SESSION['user'] = $_resultado['id_usuario'];
     header('location: ../calendario.html');
 } else {
