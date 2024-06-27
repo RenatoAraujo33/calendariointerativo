89083903e62e583c8261a205668123826351be15
Renato <rossorene4@gmail.com>
2024-06-27 20:42:19 -0300

    teste

--- PHP/logon.php
+++ PHP/logon.php
@@ -2,36 +2,8 @@
 
 include 'conexao.php';
 
-// A linha abaixo é o comando para se conectar no banco usando a função mysqli_connect
+  header('location: ../calendario.html');
 
-//$conexao = mysqli_connect($nomeServidor,$nomeUsuario, $password,$nomeBanco);
-
-// A linha abaixo é a sessão via php
-
-session_start();
-
-//essas variávaeis abaixo estão armazenando os dados vindo do usuário
-$email = $_POST['email'];
-$senha = $_POST['senha'];
-
-if ($email == "" && $senha == "" ) {
-    echo "<script> alert('Preencha os campos'); location.href='../index.html'; </script>";
-} else {
-    
-$_select="SELECT * FROM tb_usuario WHERE email = '$email'";
-$_query = mysqli_query($conexao, $_select);
-$_resultado = mysqli_fetch_array($_query);
-
-//essas duas variáveis estão armazenando o e-mail e a senha vindas do banco
-$email_banco = $_resultado['email'];
-$senha_banco = $_resultado['senha'];
-
-if ($email == $email_banco) {
-      header('location: ../calendario.html');
-} else {
-    echo "<script> alert('Usuário ou senha inválida'); location.href='../index.html'; </script>";
-}
-}
 
 
 ?>
